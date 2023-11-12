48b1d37bb2f6773a5a60551e0fe3c55215f9cc7a
Aaron Zachariah <aaron.zachariah@gmail.com>
2023-11-11 23:50:06 -0500

    initial app commmit

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/members")
+def members():
+    return { "members": ["member1", "member2", "member3"] }
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    
+ 
