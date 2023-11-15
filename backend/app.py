4894842af36afdcf1cad7292e1480e8031571535
Aaron Zachariah <aaron.zachariah@gmail.com>
2023-11-14 20:01:48 -0500

    changed frontend to vite

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+# global cors
+# CORS(app) 
+
 @app.route("/members")
+@cross_origin(origin="http://localhost:5173")
 def members():
     return { "members": ["member1", "member2", "member3"] }
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='::', port=5000)
     
  
---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  
+  return (
+    <>
+      <div>
+        {(typeof data.members === 'undefined') ? (
+          <p>Loading...</p>
+        ) : (
+          data.members.map((member, i) => (
+            <p key={i}>{member}</p>
+          ))
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
